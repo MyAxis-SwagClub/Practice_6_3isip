84ea6cb7f8e27afbb35f7b7884c4ddacb6b216dc
Артём Русаков <nicklime_forever@mail.ru>
2025-11-20 00:18:23 +0300

    По аналогии метода CreateEnemy() - Создаём метод CreateBoss() Всё работает по такому же прицинпу, лишь диапазон генерации случайного числа для переменной type увеличен, данные тоже другие.

--- Practice_6/Program.cs
+++ Practice_6/Program.cs
@@ -47,5 +47,27 @@ class SimpleRoguelike
             return new Enemy { Name = "Маг", HP = 20, Attack = 7, Type = "Маг" };
         }
     }
+    static Enemy CreateBoss()
+    {
+        int type = random.Next(4);
+        if (type == 0)
+        {
+            return new Enemy { Name = "ВВГ", HP = 60, Attack = 12, Type = "Гоблин" };
+        }
+        if (type == 1)
+        {
+            return new Enemy { Name = "Ковальский", HP = 63, Attack = 13, Type = "Скелет" };
+        }
+        if (type == 2)
+        {
+            return new Enemy { Name = "Архимаг C++", HP = 36, Attack = 11, Type = "Маг" };
+        }
+        else
+        {
+            return new Enemy { Name = "Пестов С--", HP = 26, Attack = 14, Type = "Скелет" };
+        }
+    }
+
+
 
 }
