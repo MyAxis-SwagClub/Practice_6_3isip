48870a3126e5639abbd307a0de8e32f3f2c8759b
Артём Русаков <nicklime_forever@mail.ru>
2025-11-20 00:11:36 +0300

    Создали класс Player, присвоили параметры

--- Practice_6/Program.cs
+++ Practice_6/Program.cs
@@ -2,6 +2,15 @@
 
 class SimpleRoguelike
 {
+    class Player
+    {
+        public int HP = 100;
+        public int Attack = 10;
+        public int Defense = 5;
+        public bool Frozen = false;
+    }
+
+
     static void Main()
     {
 
